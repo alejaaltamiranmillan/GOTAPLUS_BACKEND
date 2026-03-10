494f3a4158bca0cd148726fd4095c15c19070971
aleja <alejandra.altamiran01@unicatolica.edu.co>
2026-03-09 20:03:33 -0500

    fix: remover app.listen para Vercel serverless

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
-});
-
 module.exports = app;
