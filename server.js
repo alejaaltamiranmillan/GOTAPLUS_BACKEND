7c18667346716ad9140f0b03068a5ca6c0cd7f20
aleja <alejandra.altamiran01@unicatolica.edu.co>
2026-03-09 19:47:28 -0500

    fix: agregar app.listen en server.js

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const app = require('./src/app');
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
+
 module.exports = app;
