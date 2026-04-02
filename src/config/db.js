7dd2d3921ff354ec93ae28650bb6ca313aeb3e8d
aleja <alejandra.altamiran01@unicatolica.edu.co>
2026-04-02 15:08:30 -0500

    Fix: Mejorar manejo de MONGO_URI undefined

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   if (mongoose.connection.readyState >= 1) return;
+
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI is not defined in environment variables");
+  }
 
   return mongoose.connect(process.env.MONGO_URI);
 };
